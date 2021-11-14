7dee92ce5f3c0788ef23ca3077f21da920bb3d0d
gahee.han <leisure306@gmail.com>
2021-11-14 12:33:42 +0900

    add validators in login field

--- lib/ui/authentication/login.dart
+++ lib/ui/authentication/login.dart
@@ -11,6 +11,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,14 +25,17 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             _logo(),
-            Column(
-              children: [
-                _textInputWithTitle('이메일'),
-                _textInputWithTitle('비밀번호'),
-                Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: _loginButton())
-              ],
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  _textInputWithTitle('이메일'),
+                  _textInputWithTitle('비밀번호'),
+                  Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: _loginButton())
+                ],
+              ),
             ),
             const Padding(
               padding: EdgeInsets.only(top: 16.0),
@@ -59,7 +67,16 @@ class _LoginPageState extends State<LoginPage> {
     return MaterialButton(
       height: 42,
       minWidth: double.infinity,
-      onPressed: () {},
+      onPressed: () {
+        // Validate returns true if the form is valid, or false otherwise.
+        if (_formKey.currentState!.validate()) {
+          // If the form is valid, display a snackbar. In the real world,
+          // you'd often call a server or save the information in a database.
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Logging In ...')),
+          );
+        }
+      },
       child: const Text('로그인',
           style: TextStyle(
               fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
@@ -71,6 +88,13 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   _textInputWithTitle(String title) {
+    late TextFormField field;
+    if (title == '이메일') {
+      field = _emailTextFormField();
+    } else {
+      field = _passwordTextFormField();
+    }
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
       child: Column(
@@ -80,14 +104,46 @@ class _LoginPageState extends State<LoginPage> {
             padding: const EdgeInsets.only(bottom: 8.0),
             child: Text(title),
           ),
-          const TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: '',
-            ),
-          )
+          field
         ],
       ),
+    );
+  }
+
+  _emailTextFormField() {
+    return TextFormField(
+      enableSuggestions: true,
+      autocorrect: false,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '',
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "이메일을 입력해주세요";
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  _passwordTextFormField() {
+    return TextFormField(
+      obscureText: true,
+      enableSuggestions: false,
+      autocorrect: false,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '',
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "비밀번호를 입력해주세요";
+        } else {
+          return null;
+        }
+      },
     );
   }
 
@@ -96,7 +152,7 @@ class _LoginPageState extends State<LoginPage> {
       padding: EdgeInsets.all(24.0),
       child: Text("Ms. TROMM",
           style: TextStyle(
-              fontFamily: 'jalnan', fontWeight: FontWeight.bold, fontSize: 48)),
+              fontFamily: 'jalnan', fontWeight: FontWeight.bold, fontSize: 40)),
     );
   }
 }
