32324a1e55151929016f3e614bc9ab9fc999d077
gahee.han <leisure306@gmail.com>
2021-12-06 01:36:46 +0900

    rollback root nav

--- lib/ui/authentication/main_auth.dart
+++ lib/ui/authentication/main_auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ms_tromm/ui/authentication/login.dart';
 import 'package:ms_tromm/ui/authentication/tutorial.dart';
 import 'package:ms_tromm/ui/home/home.dart';
 import 'package:ms_tromm/ui/survey/survey_main.dart';
@@ -10,7 +11,7 @@ void main() {
     initialRoute: '/',
     routes: {
       // FIXME fix route later
-      '/': (context) => const HomePage(),
+      '/': (context) => const LoginPage(),
       '/survey': (context) => const SurveyMain(),
       '/home': (context) => const HomePage(),
       '/tutorial': (context) => const TutorialPage()
